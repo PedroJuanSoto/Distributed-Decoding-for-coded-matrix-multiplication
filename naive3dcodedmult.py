66bcf1db768448a6622d4508e06fc45660d1321f
pedrpan <psoto004@fiu.edu>
2019-04-13 22:35:53 -0400

    created 3-D coding (and fixed bugs in matrixmultiplication.py)

--- naive3dcodedmult.py
+++ naive3dcodedmult.py
@@ -7,20 +7,34 @@ comm = MPI.COMM_WORLD # the default communicator which consists of all the proce
 rank = comm.Get_rank() # Returns the process ID of the current process
 size = comm.Get_size() # Returns the number of processes
 
+def acode(a,i):
+    z = 0
+    for j in range(2):
+        for k in range(2):
+            z = z + a[k][j]*i**(j+2*k)
+    return z
 
+def bcode(a,i):
+    z = 0
+    for j in range(2):
+        for k in range(2):
+            z = z + a[j][k]*i**(1-j+4*k)
+    return z
 
 if rank == size-1:
 # Master work
+    decoder = np.fromfunction(lambda i,j: i**j, [9,9],dtype=int)
+    print(decoder)
     print("hola my name is ", rank)
     a = np.array([[1,2],[3,4]])
     b = np.array([[5,6],[7,8]])
+    print("the matrix A is")
     print(a)
+    print("the matrix B is")
     print(b)
-    blockmatrix =np.empty([8,2],dtype=float)
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                blockmatrix[i*4+j*2+k] = np.array([a[i][k],b[k][j]])
+    blockmatrix =np.empty([9,2],dtype=int)
+    for i in range(9):
+        blockmatrix[i] = np.array([acode(a,i),bcode(b,i)])
     print(blockmatrix)
     for i,submatrix in enumerate(blockmatrix):
          req = comm.Isend(submatrix, dest=i, tag=i)
@@ -28,19 +42,18 @@ if rank == size-1:
     result = 0
     result = comm.gather(result, root=size-1)
     print(result)
-    finalresult = np.empty([2,2],dtype=float)
-    for i in range(2):
-        for j in range(2):
-            finalresult[i][j] = result[4*i+2*j]+ result[4*i+2*j+1]
-    print(finalresult)
-
-
+    finalresult = np.matmul(np.linalg.inv(decoder),(np.delete(np.asarray(result),9)))
+    print(np.rint(finalresult))
+    print(np.delete(np.asarray(result),9))
+    c = np.array([[finalresult[1],finalresult[5]],[finalresult[3],finalresult[7]]])
+    print(np.rint(c))
 
 else:
     print("hola my name is ", rank)
-    submatrix = np.array([1,2],dtype=float)
+    submatrix = np.array([1,2],dtype=int)
     req = comm.Irecv(submatrix ,source=size-1, tag=rank)
     req.Wait()
     print(submatrix)
     result = submatrix[0]*submatrix[1]
+    print("hola my name is ", result )
     result = comm.gather(result, root=size-1)
