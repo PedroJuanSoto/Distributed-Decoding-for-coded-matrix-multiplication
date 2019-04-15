2a3fcf3d6a6d122f139edc09ac585c17d7c36473
pedrpan <psoto004@fiu.edu>
2019-04-15 15:37:43 -0400

    11 th (changed to double cleaned up old comments)

--- silent_distributed_square_3d_coded_mult.py
+++ silent_distributed_square_3d_coded_mult.py
@@ -32,14 +32,14 @@ def acode(a,i):           #This is the encoding function for the "A" matrix or
     z = 0                 #the left matrix that is going to be multiplied
     for j in range(p):
         for k in range(m):
-            z = z + a[k][j]*i**(j+2*k)
+            z = z + a[k][j]*i**(j+p*k)
     return z
 
 def bcode(a,i):          #This is the encoding function for the "B" matrix or
     z = 0                #the right matrix that is going to be multiplied
     for j in range(p):
         for k in range(n):
-            z = z + a[j][k]*i**(1-j+4*k)
+            z = z + a[j][k]*i**(p-1-j+k*p*m)
     return z
 
 def psuedo_rank(place_rank_translator, my_rank):
@@ -53,7 +53,7 @@ if rank == size-1:               # This is the master's task
     start_time = MPI.Wtime()
     a = np.arange(m*p*x*y).reshape(m,p,x,y)+1 #This is the A matrix
     b = np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1#This is the B matrix
-    blockmatrix =np.empty([size-1,2,x,y],dtype=float) #Here the master is encoding the tasks
+    blockmatrix =np.empty([size-1,2,x,y],dtype=np.double) #Here the master is encoding the tasks
     for i in range(size-1):                       #that he is going to give to the workers
         blockmatrix[i] = np.array([acode(a,i),bcode(b,i)])
     for i,submatrix in enumerate(blockmatrix):
@@ -66,27 +66,27 @@ if rank == size-1:               # This is the master's task
         data = req.wait()
         place_to_rank.append(int(data))
         comm.send(i, dest=place_to_rank[i], tag=2)
-    for i in range(fault_tolerance):        #order the workers finish in
+    for i in range(fault_tolerance):       
         comm.send(place_to_rank, dest=place_to_rank[i], tag=3)
     for i in range(size - fault_tolerance - 1):
-        req = comm.irecv(source=MPI.ANY_SOURCE, tag=1)  #That is the purpose of MPI.ANY_SOURCE
+        req = comm.irecv(source=MPI.ANY_SOURCE, tag=1)
         data = req.wait()
         place_to_rank.append(int(data))
         comm.send(i+fault_tolerance, dest=place_to_rank[i+fault_tolerance], tag=2)
-    finalresult = np.empty([fault_tolerance,x,z],dtype=float)
-    for i in range(fault_tolerance):        #order the workers finish in
+    finalresult = np.empty([fault_tolerance,x,z],dtype=np.double)
+    for i in range(fault_tolerance):
         finalresult[i] = comm.recv(source=place_to_rank[i], tag=3+3*fault_tolerance)
-    c = np.empty([m,n,x,z],dtype=float)                           #that the master must solve to decode the data. The master decodes the data by performing
-    for i in range(m):                                       #equations. the indices 1,3,5,7 contain all of the desired data, the rest is trash
+    c = np.empty([m,n,x,z],dtype=np.double)
+    for i in range(m):
         for j in range(n):
             c[i][j]=finalresult[p-1+i*p+j*p*m]
     finish_time = MPI.Wtime()
     total_time = finish_time - start_time
     print(total_time)
-    # print("yepa")
-    # print(np.rint(c))
-    # print("qepa")
-    # print(np.einsum('iksr,kjrt->ijst', np.arange(m*p*x*y).reshape(m,p,x,y)+1 , np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1))
+    print("yepa")
+    print(np.rint(c))
+    print("qepa")
+    print(np.einsum('iksr,kjrt->ijst', np.arange(m*p*x*y).reshape(m,p,x,y)+1 , np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1))
 
 
 
@@ -94,7 +94,7 @@ if rank == size-1:               # This is the master's task
 
 
 else:
-    submatrix = np.empty([2,x,y],dtype=float)           #The worker recieves the task from the master and
+    submatrix = np.empty([2,x,y],dtype=np.double)           #The worker recieves the task from the master and
     req = comm.Irecv(submatrix ,source=size-1, tag=0) #performs the multiplication assigned to him
     req.Wait()
     result = np.matmul(submatrix[0],submatrix[1])
@@ -104,7 +104,7 @@ else:
     if my_place >= fault_tolerance:
         exit()
     place_to_rank = comm.recv(source=size-1, tag=3)
-    my_row = np.fromfunction(lambda i  :  rank**i, (fault_tolerance,),dtype=float)
+    my_row = np.fromfunction(lambda i  :  rank**i, (fault_tolerance,),dtype=np.double)
     acummalator = result
     for i in range(fault_tolerance):
         if i < my_place:
--- silent_naive_square_3d_coded_mult.py
+++ silent_naive_square_3d_coded_mult.py
@@ -32,14 +32,14 @@ def acode(a,i):           #This is the encoding function for the "A" matrix or
     z = 0                 #the left matrix that is going to be multiplied
     for j in range(p):
         for k in range(m):
-            z = z + a[k][j]*i**(j+2*k)
+            z = z + a[k][j]*i**(j+p*k)
     return z
 
 def bcode(a,i):          #This is the encoding function for the "B" matrix or
     z = 0                #the right matrix that is going to be multiplied
     for j in range(p):
         for k in range(n):
-            z = z + a[j][k]*i**(1-j+4*k)
+            z = z + a[j][k]*i**(p-1-j+k*p*m)
     return z
 
 
@@ -47,42 +47,42 @@ if rank == size-1:               # This is the master's task
     start_time = MPI.Wtime()
     a = np.arange(m*p*x*y).reshape(m,p,x,y)+1 #This is the A matrix
     b = np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1#This is the B matrix
-    blockmatrix =np.empty([size-1,2,x,y],dtype=float) #Here the master is encoding the tasks
+    blockmatrix =np.empty([size-1,2,x,y],dtype=np.double) #Here the master is encoding the tasks
     for i in range(size-1):                       #that he is going to give to the workers
         blockmatrix[i] = np.array([acode(a,i),bcode(b,i)])
     for i,submatrix in enumerate(blockmatrix):
          req = comm.Isend(submatrix, dest=i, tag=0)
          req.Wait()                  #The master now broadcasts the tasks to the workers
 
-    results = np.empty([fault_tolerance,x,z],dtype=float)
+    results = np.empty([fault_tolerance,x,z],dtype=np.double)
     place_to_rank = []                      #The worker now recieves the tasks in whatever
     for i in range(fault_tolerance):        #order the workers finish in
         req = comm.irecv(source=MPI.ANY_SOURCE, tag=1)  #That is the purpose of MPI.ANY_SOURCE
         data = req.wait()
         results[i] = data["result"]
         place_to_rank.append(int(data["rank"]))
-    decoder = np.fromfunction(np.vectorize(lambda i ,j :  place_to_rank[i]**j), [fault_tolerance,fault_tolerance],dtype=int)
+    decoder = np.fromfunction(np.vectorize(lambda i ,j :  place_to_rank[int(i)]**j), (fault_tolerance,fault_tolerance),dtype=np.double)
 
-    finalresult = np.empty([fault_tolerance,x,z],dtype=float)
+    finalresult = np.empty([fault_tolerance,x,z],dtype=np.double)
     finalresult = np.einsum('ik,k...->i...', np.linalg.inv(decoder), results)
 
 
-    c = np.empty([m,n,x,z],dtype=float)                           #that the master must solve to decode the data. The master decodes the data by performing
-    for i in range(m):                                       #equations. the indices 1,3,5,7 contain all of the desired data, the rest is trash
+    c = np.empty([m,n,x,z],dtype=np.double)                          
+    for i in range(m):
         for j in range(n):
             c[i][j]=finalresult[p-1+i*p+j*p*m]
     finish_time = MPI.Wtime()
     total_time = finish_time - start_time
     print(total_time)
-    # print("yepa")
-    # print(np.rint(c))
-    # print("qepa")
-    # print(np.einsum('iksr,kjrt->ijst', np.arange(m*p*x*y).reshape(m,p,x,y)+1 , np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1))
+    print("yepa")
+    print(np.rint(c))
+    print("qepa")
+    print(np.einsum('iksr,kjrt->ijst', np.arange(m*p*x*y).reshape(m,p,x,y)+1 , np.arange(p*n*y*z).reshape(p,n,y,z)+m*p*x*y+1))
 
 
 
 else:
-    submatrix = np.empty([2,x,y],dtype=float)           #The worker recieves the task from the master and
+    submatrix = np.empty([2,x,y],dtype=np.double)           #The worker recieves the task from the master and
     req = comm.Irecv(submatrix ,source=size-1, tag=0) #performs the multiplication assigned to him
     req.Wait()
     result = np.matmul(submatrix[0],submatrix[1])
